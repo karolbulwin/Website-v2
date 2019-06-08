accabf4a62476ac77f75bf84445c453a7f87f818
Karol Bulwin <karolbulwin@gmail.com>
2019-06-08 16:41:45 +0200

    used ExampleSite

--- pages/countries/nepal.js
+++ pages/countries/nepal.js
@@ -1,19 +1,13 @@
 import Layout from "../../components/MyLayout.js";
 import Head from "../../components/Head.js";
+import ExampleSite from "../../components/ExampleSite";
 
 const Index = () => (
 	<>
 		<Head title="Nepal" />
 		<Layout>
 			<div className="col-sm-9 col-md-8 order-sm-2 order-lg-first py-5 d-flex justify-content-center align-self-center text-center">
-				<section className="jackInTheBox animated">
-					<h1 className="display-5 text-center p-3 ">
-						This was just an example of a website
-					</h1>
-					<button type="button" className="btn btn-success btn-back">
-						Go back
-					</button>
-				</section>
+				<ExampleSite />
 			</div>
 		</Layout>
 	</>
