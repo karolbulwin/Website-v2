1c9e745a399686d6323bc21e359d8d6aca0d60a8
Karol Bulwin <karolbulwin@gmail.com>
2019-06-08 16:40:25 +0200

    ExampleSite component

---
+++ components/ExampleSite.js
@@ -0,0 +1,12 @@
+import GoBackButton from "../components/GoBackButton";
+
+const ExampleSite = () => (
+	<section>
+		<h1 className="display-5 text-center p-3 ">
+			This was just an example of a website
+		</h1>
+		<GoBackButton />
+	</section>
+);
+
+export default ExampleSite;
