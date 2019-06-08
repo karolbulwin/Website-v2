a5717eaf236a8bbcee1903a18fe9a4340590aa66
Karol Bulwin <karolbulwin@gmail.com>
2019-06-08 20:55:05 +0200

    add keyboard custom support

--- components/MyLayout.js
+++ components/MyLayout.js
@@ -4,30 +4,46 @@ import Footer from "./Footer";
 import AsideCountries from "./AsideCountries";
 import AsideActivities from "./AsideActivities";
 
-const Layout = ({ children, hide }) => (
-	<>
-		<Header />
-		<main className="container-fluid mt-5em">
-			<div className="row">
-				{children}
-				{hide === "countries" ? null : <AsideCountries />}
-				{hide === "activities" ? null : <AsideActivities />}
-			</div>
-			<style jsx>{`
-				.mt-5em {
-					margin-top: 4.5em;
-				}
-			`}</style>
-			<style global jsx>{`
-				:root {
-					--mainColor: #333;
-					--asideColor: #e6e6e6;
-					--asideTitle: black;
-				}
-				* {
-					box-sizing: border-box;
-				}
-				html {
+const Layout = ({ children, hide }) => {
+	useEffect(() => {
+		function usingMouse() {
+			document.body.classList.add("using-mouse");
+		}
+		function usingKeybord() {
+			document.body.classList.remove("using-mouse");
+		}
+		document.body.addEventListener("mousedown", usingMouse);
+		document.body.addEventListener("keydown", usingKeybord);
+
+		return () => {
+			document.body.removeEventListener("mousedown", usingMouse);
+			document.body.removeEventListener("keydown", usingKeybord);
+		};
+	});
+	return (
+		<>
+			<Header />
+			<main className="container-fluid mt-5em">
+				<div className="row">
+					{children}
+					{hide === "countries" ? null : <AsideCountries />}
+					{hide === "activities" ? null : <AsideActivities />}
+				</div>
+				<style jsx>{`
+					.mt-5em {
+						margin-top: 4.5em;
+					}
+				`}</style>
+				<style global jsx>{`
+					:root {
+						--mainColor: #333;
+						--asideColor: #e6e6e6;
+						--asideTitle: black;
+					}
+					* {
+						box-sizing: border-box;
+					}
+					html {
 					font-family: 'Open Sans', sans-serif
 					font-size: 1em;
 					position: relative;
@@ -45,6 +61,15 @@ const Layout = ({ children, hide }) => (
 				p {
 					user-select: text;
 				}
+
+					.using-mouse :focus:not(:disabled) {
+						outline: none;
+					}
+					:focus:not(:disabled) {
+						outline-color: rgba(255, 145, 0, 0.5);
+						outline-style: solid;
+						outline-width: 0.125em;
+					}
 			`}</style>
 		</main>
 		<Footer />
